c3a66103a2bba1a0d024f809f7d1266b2a4d7fd4
Juntars <funkinagoti@gmail.com>
2025-04-04 09:30:00 +0200

    Update ryby.php

--- ryby.php
+++ ryby.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";      
 $password = "";          
-$dbname = "wedkowanie"; 
+$dbname = "ryby"; 
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
